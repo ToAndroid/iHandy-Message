fb58aa128c1e0a095400662172e7f3f43afc5afd
hqythu <hqythu@gmail.com>
2015-09-05 19:59:56 +0800

    use viewpager in demo.mainactivity

--- app/src/main/java/com/ihs/demo/message/MainActivity.java
+++ app/src/main/java/com/ihs/demo/message/MainActivity.java
@@ -1,72 +1,100 @@
 package com.ihs.demo.message;
 
+import me.hqythu.ihs.message.ui.BaseActivity;
 import test.contacts.demo.friends.api.HSContactFriendsMgr;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBar.Tab;
 import android.support.v7.app.ActionBar.TabListener;
+import android.support.v7.widget.Toolbar;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.astuetz.PagerSlidingTabStrip;
 import com.ihs.commons.utils.HSLog;
 import com.ihs.message_2012010548.managers.HSMessageManager;
 
+import java.util.ArrayList;
+
 import me.hqythu.ihs.message.R;
 
-public class MainActivity extends HSActionBarActivity {
+public class MainActivity extends BaseActivity {
 
     private final static String TAG = MainActivity.class.getName();
-    private Tab tabs[];
+    private PagerSlidingTabStrip tabs;
+    private ViewPager mViewPager;
+    private ArrayList<Fragment> fragments = new ArrayList<>();
+
+    private class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final String[] TITLES = {
+            getString(R.string.contacts),
+            getString(R.string.messages),
+            getString(R.string.settings),
+            getString(R.string.sample)
+        };
+
+        public ViewPagerAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
+        }
+
+        @Override
+        public Fragment getItem(int Index) {
+            return fragments.get(Index);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return TITLES[position];
+        }
+
+        @Override
+        public int getCount() {
+            return TITLES.length;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.ihs_activity_main);
 
-        ActionBar bar = this.getSupportActionBar();
-        bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-        int[] tabNames = { R.string.contacts, R.string.messages, R.string.settings, R.string.sample };
-        tabs = new Tab[4];
-        for (int i = 0; i < 4; i++) {
-            Tab tab = bar.newTab();
-            tabs[i] = tab;
-            tab.setText(tabNames[i]);
-            tab.setTabListener(new TabListener() {
-
-                @Override
-                public void onTabUnselected(Tab arg0, FragmentTransaction arg1) {
-                    HSLog.d(TAG, "unselected " + arg0);
-                }
-
-                @Override
-                public void onTabSelected(Tab arg0, FragmentTransaction arg1) {
-                    HSLog.d(TAG, "selected " + arg0);
-                    if (tabs[0] == arg0) {
-                        Fragment f = new ContactsFragment();
-                        arg1.replace(android.R.id.content, f);
-                    } else if (tabs[1] == arg0) {
-                        Fragment f = new MessagesFragment();
-                        arg1.replace(android.R.id.content, f);
-                    } else if (tabs[2] == arg0) {
-                        Fragment f = new SettingsFragment();
-                        arg1.replace(android.R.id.content, f);
-                    } else if (tabs[3] == arg0) {
-                        Fragment f = new SampleFragment();
-                        arg1.replace(android.R.id.content, f);
-                    }
-                }
-
-                @Override
-                public void onTabReselected(Tab arg0, FragmentTransaction arg1) {
-                    HSLog.d(TAG, "reselected " + arg0);
-                }
-            });
-            bar.addTab(tab);
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.ihs_toolbar);
+        setSupportActionBar(mToolbar);
+        mToolbar.setBackgroundColor(getResources().getColor(R.color.primary_green));
+        mToolbar.setTitle("Message");
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
         }
 
-        setContentView(R.layout.ihs_activity_main);
+        tabs = (PagerSlidingTabStrip) findViewById(R.id.ihs_tabs);
+        mViewPager = (ViewPager) findViewById(R.id.ihs_viewpager);
+
+        ContactsFragment contacts = new ContactsFragment();
+        fragments.add(contacts);
+        MessagesFragment messages = new MessagesFragment();
+        fragments.add(messages);
+        SettingsFragment settings = new SettingsFragment();
+        fragments.add(settings);
+        SampleFragment sample = new SampleFragment();
+        fragments.add(sample);
+
+        mViewPager.setAdapter(new ViewPagerAdapter(getSupportFragmentManager()));
+        mViewPager.setCurrentItem(0);
+
+        tabs.setShouldExpand(true);
+        tabs.setTabPaddingLeftRight(8);
+        tabs.setViewPager(mViewPager);
+        tabs.setBackgroundColor(getResources().getColor(R.color.primary_green));
+        tabs.setIndicatorColor(getResources().getColor(R.color.primary_dark_green));
+        tabs.setTextColor(getResources().getColor(R.color.WHITE));
+        tabs.setTextSize((int) (16 * getResources().getDisplayMetrics().density));
     }
 
     @Override
@@ -82,8 +110,9 @@ public class MainActivity extends HSActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case R.id.action_settings:
+                return true;
         }
         return super.onOptionsItemSelected(item);
     }
@@ -98,7 +127,6 @@ public class MainActivity extends HSActionBarActivity {
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_BACK) {
             moveTaskToBack(false);
-            return true;
         }
         return super.onKeyDown(keyCode, event);
     }
